d6c03c52bd4940ce74d3b871fbd45aab865e362e
sja_thedude <sja.affu765@gmail.com>
2021-11-22 19:22:30 +0300

    Files added with Components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
+/* eslint-disable react/prefer-stateless-function */
+import React, { Component } from 'react';
+import Calculator from './components/Calculator';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Math Magicians
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;
