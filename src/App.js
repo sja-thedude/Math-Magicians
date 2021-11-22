9c4fd2e75c903f3cb677e1ccb9e787959a42800a
sja_thedude <sja.affu765@gmail.com>
2021-11-22 21:13:46 +0300

    Updated with linters

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Calculator from './components/Calculator';
 import './App.css';
 
-const App = () => {
-    return (
-      <div className="App">
-        <Calculator />
-      </div>
-    );
-}
+const App = () => (
+  <div className="App">
+    <Calculator />
+  </div>
+);
 
 export default App;
