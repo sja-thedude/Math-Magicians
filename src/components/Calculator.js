697059ffab36d2d28f066db4e9cbdc5a9bcef61c
sja_thedude <sja.affu765@gmail.com>
2021-11-23 13:01:43 +0300

    updated with linters

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from 'react';
 import View from './View';
 import Button from './Button';
@@ -19,25 +18,25 @@ const Calculator = () => {
   return (
     <div id="calculator">
       <View vId="display" total={total} next={next} operation={operation} />
-      <Button bId="clear" bName="AC" handleClick={this.handleClick} />
-      <Button bId="del" bName="+/-" handleClick={this.handleClick} />
-      <Button bId="del" bName="%" handleClick={this.handleClick} />
-      <Button bId="div" bName="÷" handleClick={this.handleClick} />
-      <Button bId="seven" bName="7" handleClick={this.handleClick} />
-      <Button bId="eight" bName="8" handleClick={this.handleClick} />
-      <Button bId="nine" bName="9" handleClick={this.handleClick} />
-      <Button bId="mul" bName="x" handleClick={this.handleClick} />
-      <Button bId="four" bName="4" handleClick={this.handleClick} />
-      <Button bId="five" bName="5" handleClick={this.handleClick} />
-      <Button bId="six" bName="6" handleClick={this.handleClick} />
-      <Button bId="sub" bName="-" handleClick={this.handleClick} />
-      <Button bId="one" bName="1" handleClick={this.handleClick} />
-      <Button bId="two" bName="2" handleClick={this.handleClick} />
-      <Button bId="three" bName="3" handleClick={this.handleClick} />
-      <Button bId="add" bName="+" handleClick={this.handleClick} />
-      <Button bId="zero" bName="0" handleClick={this.handleClick} />
-      <Button bId="decimal" bName="." handleClick={this.handleClick} />
-      <Button bId="equal" bName="=" handleClick={this.handleClick} />
+      <Button bId="clear" bName="AC" handleClick={handleClick} />
+      <Button bId="del" bName="+/-" handleClick={handleClick} />
+      <Button bId="del" bName="%" handleClick={handleClick} />
+      <Button bId="div" bName="÷" handleClick={handleClick} />
+      <Button bId="seven" bName="7" handleClick={handleClick} />
+      <Button bId="eight" bName="8" handleClick={handleClick} />
+      <Button bId="nine" bName="9" handleClick={handleClick} />
+      <Button bId="mul" bName="x" handleClick={handleClick} />
+      <Button bId="four" bName="4" handleClick={handleClick} />
+      <Button bId="five" bName="5" handleClick={handleClick} />
+      <Button bId="six" bName="6" handleClick={handleClick} />
+      <Button bId="sub" bName="-" handleClick={handleClick} />
+      <Button bId="one" bName="1" handleClick={handleClick} />
+      <Button bId="two" bName="2" handleClick={handleClick} />
+      <Button bId="three" bName="3" handleClick={handleClick} />
+      <Button bId="add" bName="+" handleClick={handleClick} />
+      <Button bId="zero" bName="0" handleClick={handleClick} />
+      <Button bId="decimal" bName="." handleClick={handleClick} />
+      <Button bId="equal" bName="=" handleClick={handleClick} />
     </div>
   );
 };
