5526a34f2637ba30848e79c499933149c08ff307
sja_thedude <sja.affu765@gmail.com>
2021-11-30 18:41:01 +0300

    Files added for unit tests and snapshots

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  render(
+    <Router>
+      <App />
+    </Router>,
+  );
 });
